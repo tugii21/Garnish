f1ccf3f01b622d1a92c26f731f0277df10cf2385
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-03-14 11:19:28 +0000

    static file added

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -139,6 +139,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
