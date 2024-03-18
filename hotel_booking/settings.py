73cc2b6b3fcb7b9bc3522e94105412a6de499d34
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-03-18 12:40:58 +0000

    nuuts tvlxvvriiig env rvv xuulsan

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n-m2i23eho5^kl9s3tl7y7$93##1((9fo^5_ztd%two_%djj%7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
