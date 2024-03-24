a24b31c1f11f8c22be7f7fa2bd6acdf0b0178926
tugii21 bat <batsaikhantuguldur21@gmail.com>
2024-03-24 03:21:12 +0000

    collect static file

--- staticfiles/jss/alert.js
+++ staticfiles/jss/alert.js
@@ -1,9 +1,10 @@
 // Get the alert element
-var alert = document.getElementById('loginAlert');
+var alertElement = document.getElementById("loginAlert");
 
-//  click event listener to the alert element, code allows the user to interact with the alert element by clicking on
-alert.addEventListener('click', function (event) {
-    if (event.target.classList.contains('btn-close') || event.target.id === 'okButton') {
-        alert.classList.remove('show');
+// Click event listener to the alert element
+// code allows the user to interact with the alert element by clicking on
+alertElement.addEventListener("click", function (event) {
+    if (event.target.classList.contains("btn-close") || event.target.id === "okButton") {
+        alertElement.classList.remove("show");
     }
 });
